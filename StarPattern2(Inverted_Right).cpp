605c7b57ff896dd71452b568803388c0c94b51bd
Shubham Balasaheb Bhavar <shubhambhavar108@gmail.com>
2026-07-28 14:46:55 +0530

    Add Inverted Right Triangle Star Pattern program
    This program prints an inverted right triangle star pattern with 5 rows.

---
+++ StarPattern2(Inverted_Right).cpp
@@ -0,0 +1,32 @@
+/*
+----------------------------------------
+Program: Inverted Right Triangle Star Pattern
+
+Output:
+* * * * *
+* * * *
+* * *
+* *
+*
+
+----------------------------------------
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i, j;
+
+    for(i = 5; i >= 1; i--)
+    {
+        for(j = 1; j <= i; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
